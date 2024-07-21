1a245fe3c35dd40d40fdeadff9fc882d420df6e4
Antone Gonzales <es.angelika@gmail.com>
2024-07-21 17:31:33 -0500

    es de prueba

--- dev013-movie-challenge/src/components/Menu.tsx
+++ dev013-movie-challenge/src/components/Menu.tsx
@@ -1,7 +1,7 @@
 const Menu = () => {
   return (
     <div>
-      <p>Aqui ira el menu desplegable con las opciones</p>
+      <p>Aqui ira el menu desplegable con las opciones, haciendo pruebas</p>
     </div>
   )
 }
