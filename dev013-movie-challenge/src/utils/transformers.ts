40d53783635d16a2512612a78ecceb5c402f3995
Antone Gonzales <es.angelika@gmail.com>
2024-07-22 10:01:08 -0500

    se agrego el archivo de vercel para el deploy fixes #6

---
+++ dev013-movie-challenge/src/utils/transformers.ts
@@ -0,0 +1,40 @@
+import Movie  from "../models/Movie"
+
+// Definir el tipo de datos que recibo de la API
+export interface MovieData {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: number[];
+    id: number;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+  }    
+  /**
+ * Transformar los datos de la API en un objeto Movie.
+ *
+ * @param data - Datos de película de la API.
+ * @param genresNames - Mapa de IDs de géneros a nombres de géneros.
+ * @returns Un objeto Movie con los datos transformados.
+ */
+
+  export function formatMovie(data: MovieData, genresNames: Map<number, string>): Movie {
+    const movie: Movie = {
+      id: data.id,
+      title: data.title,
+      year: data.release_date ? data.release_date.slice(0, 4) : "", // Extrae el año de la fecha de lanzamiento
+      posterPath: data.poster_path ? `https://image.tmdb.org/t/p/w500${data.poster_path}` : "", // Forma la URL del póster
+      genres: data.genre_ids ? data.genre_ids.map((n: number) => genresNames.get(n) || "Unknown") : [], // Mapea los IDs de géneros a nombres
+      overview: data.overview,
+      voteAverage: data.vote_average,
+      voteCount: data.vote_count
+    };
+    return movie;
+  }
