af69349d43aa9c0c060d786c502c355b931354ae
Antone Gonzales <es.angelika@gmail.com>
2024-08-05 11:29:26 -0500

    Modifique la funcion getMovies usando la funcion formatMovie

--- dev013-movie-challenge/src/services/APIService.ts
+++ dev013-movie-challenge/src/services/APIService.ts
@@ -1,34 +1,35 @@
 import { Movie } from "../models/Movie";
 import { apiKey } from "../utils/getEnv";
+import { formatMovie, formatGenresToMap } from "../utils/transformers";
 
 export interface Results {
   movies: Movie[];
 }
+export interface Genre {
+  id: number;
+  name: string;
+}
 
 export const getMovie = async (): Promise<Results> => {
   const apiKeyP = apiKey();
-  const url=`https://api.themoviedb.org/3/discover/movie?api_key=${apiKeyP}`;
+  const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKeyP}`;
   console.log("API Key:", apiKeyP);
   console.log("Fetch URL:", url);
-  const response = await fetch(url,{
-      headers: {
-        "Authorization": `Bearer ${apiKeyP}`,
-      },
-    }
-  );
-  if (!response.ok){
-    throw new Error("Solicitud no exitosa")
+  const response = await fetch(url, {
+    headers: {
+      "Authorization": `Bearer ${apiKeyP}`,
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Solicitud no exitosa");
   }
   const data = await response.json();
 
-  const movies = data.results.map((item: any) => ({
-    id: item.id,
-    title: item.title,
-    year: item.release_date.split("-")[0],
-    genre: item.genre_ids.join(", "), // Adaptar esto según cómo se obtienen los géneros
-    poster: item.poster_path,
-  }));
+  const genres = data.genres || [];
+  const genresMap = formatGenresToMap(genres);
+
+  // Usar la función formatMovie para formatear los datos de las películas
+  const movies = data.results.map((item: any) => formatMovie(item, genresMap));
 
   return { movies };
-
 };
